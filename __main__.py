f2ff7741d7cb39ce2820f084a4d706b3f4abd46c
Thomas Smith <thsm@umich.edu>
2019-12-14 17:27:19 -0500

    Implement better command line interface

--- __main__.py
+++ __main__.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import sys
 
 import library
 
-if len(sys.argv) != 2:
-  program_name = "scraper"
-  if len(sys.argv) >= 1:
-    program_name = sys.argv[0]
-  print("Usage: ", program_name, " <scraping spec file>")
-  sys.exit(1)
+parser = argparse.ArgumentParser(
+  description="Scrape the contents of an HTML page."
+)
+parser.add_argument(
+  "scraping_spec_file",
+  type=argparse.FileType("r"),
+  nargs="?",
+  default=sys.stdin,
+  help="a JSON file containing information about what to scrape"
+)
+parser.add_argument(
+  "--pretty-print",
+  action="store_true",
+  help="enable pretty-printing of JSON output"
+)
+args = parser.parse_args()
+scraping_spec_file = args.scraping_spec_file
+should_pretty_print = args.pretty_print
 
-[_, scraping_spec_file] = sys.argv
-
-scraping_spec_file = open(scraping_spec_file)
 scraping_spec = json.load(scraping_spec_file)
 scraping_spec_file.close()
 
@@ -29,4 +39,7 @@ scraping_spec_file should be a JSON file in the format:
 
 result = library.scrape(scraping_spec)
 
-print(json.dumps(result))
+if should_pretty_print:
+  print(json.dumps(result, indent=4))
+else:
+  print(json.dumps(result))
