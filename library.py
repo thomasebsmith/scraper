d07c281d133ff7ce5748a1a5d451b89068d651e9
Thomas Smith <thsm@umich.edu>
2019-12-21 09:34:10 -0500

    Fix behavior when no elements match a selector

--- library.py
+++ library.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
-def parse_as(value, type_string):
+def __parse_as(value, type_string):
   converter = str
   if type_string == "int":
     converter = int
@@ -16,44 +16,47 @@ def parse_as(value, type_string):
   except ValueError:
     return None
 
-def scrape(spec):
-  def recursive_scrape(scrape, html):
-    options = {}
+def __recursive_scrape(scrape, html):
+  options = {}
+  result = None
+
+  if type(scrape) is dict:
+    options = scrape
+    scrape = scrape["type"]
+
+  if scrape == "html":
+    result = str(html)
+  elif scrape == "text":
+    result = html.get_text()
+  elif scrape == "text_nodes":
+    # TODO
     result = None
+  elif scrape == "attribute":
+      result = html.attrs.get(options["attribute"])
 
-    if type(scrape) is dict:
-      options = scrape
-      scrape = scrape["type"]
+  if result != None or type(scrape) != list:
+    if "regex" in options:
+      pattern = re.compile(options["regex"][0])
+      result = pattern.sub(options["regex"][1], result)
+    result = __parse_as(result, options.get("parse_as", "str"))
+    return result
 
-    if scrape == "html":
-      result = str(html)
-    elif scrape == "text":
-      result = html.get_text()
-    elif scrape == "text_nodes":
-      # TODO
-      result = None
-    elif scrape == "attribute":
-       result = html.attrs.get(options["attribute"])
+  # else: scrape should be an array of elements
+  results = {}
+  for el in scrape:
+    to_scrape = el["scrape"]
+    tags = html.select(el["selector"])
+    if not el["select_all"] and len(tags) > 0:
+      tag = tags[0]
+      results[el["key"]] = __recursive_scrape(to_scrape, tag)
+    elif not el["select_all"]:
+      results[el["key"]] = None
+    else:
+      results[el["key"]] = [__recursive_scrape(to_scrape, tag) for tag in tags]
+  return results
 
-    if result != None or type(scrape) != list:
-      if "regex" in options:
-        pattern = re.compile(options["regex"][0])
-        result = pattern.sub(options["regex"][1], result)
-      result = parse_as(result, options.get("parse_as", "str"))
-      return result
 
-    # else: scrape should be an array of elements
-    results = {}
-    for el in scrape:
-      to_scrape = el["scrape"]
-      tags = html.select(el["selector"])
-      if not el["select_all"] and len(tags) > 0:
-        tag = tags[0]
-        results[el["key"]] = recursive_scrape(to_scrape, tag)
-      else:
-        results[el["key"]] = [recursive_scrape(to_scrape, tag) for tag in tags]
-    return results
-
+def scrape(spec):
   url = spec["url"]
   should_scrape_all = spec["scrape_all"]
 
@@ -64,4 +67,4 @@ def scrape(spec):
   html = BeautifulSoup(req.text, "html.parser")
   if type(elements) != list:
     return None
-  return recursive_scrape(elements, html)
+  return __recursive_scrape(elements, html)
